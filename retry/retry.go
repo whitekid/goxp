3d6045daab105e041512bcc7586a502a98811b15
Charlie.Choe <whitekid@gmail.com>
2019-03-12 14:26:11 +0900

    add retry

---
+++ retry/retry.go
@@ -0,0 +1,61 @@
+package retry
+
+import (
+	"log"
+	"time"
+)
+
+// Retry return new default retrier
+func Retry() Retrier {
+	return &retrier{
+		limit:          5,
+		initialBackoff: time.Millisecond * 100,
+		backoffRatio:   1.3,
+	}
+}
+
+// Retrier is retrier interface
+type Retrier interface {
+	// total try limit
+	Limit(limit int) Retrier
+
+	// backoff settings when fails
+	Backoff(initial time.Duration, backoffRatio float64) Retrier
+
+	// run the func
+	Do(func() error) error
+}
+
+type retrier struct {
+	limit          int
+	initialBackoff time.Duration
+	backoffRatio   float64
+}
+
+func (r *retrier) Limit(limit int) Retrier {
+	r.limit = limit
+	return r
+}
+
+func (r *retrier) Backoff(initial time.Duration, ratio float64) Retrier {
+	r.initialBackoff = initial
+	r.backoffRatio = ratio
+	return r
+}
+
+func (r *retrier) Do(fn func() error) (err error) {
+	backoff := r.initialBackoff
+
+	for i := 0; i < r.limit; i++ {
+		if err = fn(); err != nil {
+			log.Printf("try %d failed, retry in %s", i+1, backoff)
+			time.Sleep(backoff)
+			backoff = time.Duration(float64(backoff) * r.backoffRatio)
+			continue
+		}
+
+		return nil
+	}
+
+	return err
+}
---
+++ retry/retry_test.go
@@ -0,0 +1,45 @@
+package retry
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRetry(t *testing.T) {
+	type args struct {
+		limit   int
+		initial time.Duration
+		backoff float64
+		fn      func() error
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		tries   int
+	}{
+		{"default", args{3, time.Millisecond * 100, 1.3, func() error { return nil }}, false, 1},
+		{"error", args{3, time.Millisecond * 100, 1.3, func() error { return errors.New("fail") }}, true, 3},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tries := 0
+			err := Retry().Limit(tt.args.limit).Backoff(tt.args.initial, tt.args.backoff).
+				Do(func() error {
+					tries++
+					return tt.args.fn()
+				})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("retry error=%v, want=%v", err, tt.wantErr)
+				return
+			}
+
+			require.Equal(t, tt.tries, tries)
+		})
+	}
+}
