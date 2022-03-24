eb3d106b7e9ae8edbbd9202a23b197c07a7df674
Choe, Cheng-Dae <whitekid@gmail.com>
2022-03-24 09:40:46 +0900

    add run every

--- worker.go
+++ worker.go
@@ -1,6 +1,12 @@
 package utils
 
-import "sync"
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/whitekid/go-utils/log"
+)
 
 // DoWithWorker iterate chan and run do() with n workers
 func DoWithWorker(workers int, gen func(), do func(i int)) {
@@ -16,4 +22,22 @@ func DoWithWorker(workers int, gen func(), do func(i int)) {
 	}
 
 	wg.Wait()
+}
+
+type EveryFunc func() error
+
+// Every execute fn() in every interval
+func Every(ctx context.Context, interval time.Duration, fn EveryFunc) {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(interval):
+				if err := fn(); err != nil {
+					log.Errorf("task failed with %+v", err)
+				}
+			}
+		}
+	}()
 }
