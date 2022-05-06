c24c08d1c425371585a3665bfc4976992ed63e93
Choe, Cheng-Dae <whitekid@gmail.com>
2022-05-06 15:40:40 +0900

    add strings tests

--- types/strings.go
+++ types/strings.go
@@ -4,29 +4,34 @@ import (
 	"io"
 	"sort"
 	"strings"
+
+	"github.com/whitekid/goxp/fx"
 )
 
 // Strings represents string array
 type Strings []string
 
+func NewStrings(s []string) *Strings { r := Strings(s); return &r }
+
 // Slice ...
-func (s *Strings) Slice() []string {
-	return []string(*s)
+func (s Strings) Slice() []string {
+	return []string(s)
 }
 
 // Append new string
-func (s *Strings) Append(e ...string) {
+func (s *Strings) Append(e ...string) *Strings {
 	*s = append(*s, e...)
+	return s
 }
 
 // Contains returns if strings contains s1
-func (s *Strings) Contains(s1 string) bool {
+func (s Strings) Contains(s1 string) bool {
 	return s.Index(s1) >= 0
 }
 
 // Index returns index of s1
-func (s *Strings) Index(s1 string) int {
-	for i, e := range *s {
+func (s Strings) Index(s1 string) int {
+	for i, e := range s {
 		if e == s1 {
 			return i
 		}
@@ -35,9 +40,9 @@ func (s *Strings) Index(s1 string) int {
 }
 
 // Copy return copied string array
-func (s *Strings) Copy() *Strings {
-	sl := Strings(make([]string, len(*s)))
-	copy(sl, *s)
+func (s Strings) Copy() *Strings {
+	sl := Strings(make([]string, len(s)))
+	copy(sl, s)
 
 	return &sl
 }
@@ -54,18 +59,33 @@ func (s *Strings) Remove(e string) *Strings {
 
 // RemoveAt remove indexed string
 func (s *Strings) RemoveAt(i int) *Strings {
-	sl := append((*s)[:i], (*s)[i+1:]...)
-	return &sl
+	*s = Strings(append((*s)[:i], (*s)[i+1:]...))
+	return s
 }
 
 // Equals return true if equals else return false
-func (s *Strings) Equals(s1 Strings) bool {
-	if len(*s) != len(s1) {
+func (s Strings) Equals(s1 Strings) bool {
+	if len(s) != len(s1) {
 		return false
 	}
 
-	for i := range *s {
-		if (*s)[i] != s1[i] {
+	for i := range s {
+		if s[i] != s1[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// EqualFold return true if equalFold else return false
+func (s Strings) EqualFold(s1 Strings) bool {
+	if len(s) != len(s1) {
+		return false
+	}
+
+	for i := range s {
+		if strings.EqualFold(s[i], s1[i]) {
 			return false
 		}
 	}
@@ -74,42 +94,52 @@ func (s *Strings) Equals(s1 Strings) bool {
 }
 
 // ToInterface returns []interfaces{}
-func (s *Strings) ToInterface() (result []interface{}) {
-	result = make([]interface{}, len(*s))
-	for i := 0; i < len(*s); i++ {
-		result[i] = (*s)[i]
+func (s Strings) ToInterface() (result []interface{}) {
+	result = make([]interface{}, len(s))
+	for i := 0; i < len(s); i++ {
+		result[i] = s[i]
 	}
 
 	return
 }
 
 // Reader returns new concat readers
-func (s *Strings) Reader(sep string) io.Reader {
-	n := len(*s)
+func (s Strings) Reader(sep string) io.Reader {
+	n := len(s)
 	switch n {
 	case 0:
 		return strings.NewReader("")
 	case 1:
-		return strings.NewReader((*s)[0])
+		return strings.NewReader(s[0])
+	}
+
+	if sep == "" {
+		rs := fx.Map(s, func(x string) io.Reader { return strings.NewReader(x) })
+		return io.MultiReader(rs...)
 	}
 
 	readers := make([]io.Reader, n*2-1)
 
-	readers[0] = strings.NewReader((*s)[0])
+	readers[0] = strings.NewReader(s[0])
 	for i := 1; i < n; i++ {
 		readers[i*2-1] = strings.NewReader(sep)
-		readers[i*2] = strings.NewReader((*s)[i])
+		readers[i*2] = strings.NewReader(s[i])
 	}
 
 	return io.MultiReader(readers...)
 }
 
 // Join ...
-func (s *Strings) Join(sep string) string {
-	return strings.Join([]string(*s), sep)
+func (s Strings) Join(sep string) string {
+	return strings.Join(s.Slice(), sep)
 }
 
 // Sort string array
-func (s *Strings) Sort() {
-	sort.Strings(*s)
+func (s Strings) Sort() {
+	sort.Strings(s)
+}
+
+func (s Strings) Map(f func(s string) string) *Strings {
+	sl := Strings(fx.Map(s, func(e string) string { return f(e) }))
+	return &sl
 }
