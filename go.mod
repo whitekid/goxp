b6589fcf17781eecde103a10a3e7ebb544fa29c0
Choe, Cheng-Dae <whitekid@gmail.com>
2022-09-05 16:19:17 +0900

    echo helpers

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/whitekid/goxp
 go 1.19
 
 require (
+	github.com/go-playground/validator/v10 v10.11.0
 	github.com/google/uuid v1.3.0
+	github.com/labstack/echo/v4 v4.9.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
@@ -16,9 +18,16 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
@@ -27,10 +36,14 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ helpers/echo/echo.go
@@ -0,0 +1,103 @@
+package echo
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var validate = validator.New()
+
+// NewEcho create new default echo handlers
+//
+// TODO move to goxp
+func NewEcho(middlewares ...echo.MiddlewareFunc) *echo.Echo {
+	e := echo.New()
+	e.HideBanner = true
+	e.Validator = &Validator{validator: validate}
+	e.Use(middleware.Logger())
+	e.Use(LogErrors())
+	e.Use(middlewares...)
+
+	return e
+}
+
+// Middlewares
+
+// LogErrors log error when http status error occurred
+func LogErrors() echo.MiddlewareFunc { return LogErrorsWithCode(http.StatusBadGateway) }
+func LogErrorsWithCode(logCode int) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		// log http errors
+		return func(c echo.Context) error {
+			err := next(c)
+			if err != nil {
+				code := http.StatusInternalServerError
+
+				if ee, ok := err.(validator.ValidationErrors); ok {
+					err = echo.NewHTTPError(http.StatusBadRequest, ee.Error())
+				}
+
+				if he, ok := err.(*echo.HTTPError); ok {
+					code = he.Code
+				}
+
+				if code >= logCode {
+					c.Logger().Errorf("%+v", err)
+				}
+			}
+
+			return err
+		}
+	}
+}
+
+func ExtractHeader(header string, fn func(c echo.Context, cval string)) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fn(c, c.Request().Header.Get(header))
+
+			return next(c)
+		}
+	}
+}
+
+// ExtractParam extract path parameter and callback to use custom context
+// Usage:
+// 	e.Use(ExtractParam("project_id", func(c echo.Context, val string) { c.(*Context).projectID = val }))
+
+func ExtractParam(param string, callback func(c echo.Context, val string)) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			callback(c, c.Param(param))
+
+			return next(c)
+		}
+	}
+}
+
+// Bind bind & validate
+func Bind(c echo.Context, val interface{}) error {
+	if err := c.Bind(val); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	if err := c.Validate(val); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Validator struct {
+	validator *validator.Validate
+}
+
+func (v *Validator) Validate(i interface{}) error {
+	if err := v.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
