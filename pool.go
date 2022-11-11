2ff9f5b58270848a36d1f8f52c2bcbb21e77761c
Choe, Cheng-Dae <whitekid@gmail.com>
2022-11-11 09:00:08 +0900

    add Pool

---
+++ pool.go
@@ -0,0 +1,18 @@
+package goxp
+
+import "sync"
+
+type Pool[T any] struct {
+	sync.Pool
+}
+
+func NewPool[T any](New func() T) *Pool[T] {
+	pool := &Pool[T]{}
+	if New != nil {
+		pool.New = func() any { return New() }
+	}
+	return pool
+}
+
+func (p *Pool[T]) Put(x T) { p.Pool.Put(x) }
+func (p *Pool[T]) Get() T  { return p.Pool.Get().(T) }
