f8f467cb6543b986296209c6642ee3cac7fa0a0e
Choe, Cheng-Dae <whitekid@gmail.com>
2023-02-17 01:55:54 +0900

    add ValidContext()

--- context.go
+++ context.go
@@ -3,11 +3,17 @@ package goxp
 import "context"
 
 // IsContextDone return true if context is done
+// Depreciated: use ValidContext()
 func IsContextDone(ctx context.Context) bool {
+	return !ValidContext(ctx)
+}
+
+// ValidContext return true if context is valid(not canceled or deadline exceed)
+func ValidContext(ctx context.Context) bool {
 	select {
 	case <-ctx.Done():
-		return true
-	default:
 		return false
+	default:
+		return true
 	}
 }
