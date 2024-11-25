4d98fc378963df5f2aa40fa7cd79f6b851b105cc
Choe, Cheng-Dae <whiteid@gmail.com>
2024-11-25 12:04:31 +0900

    simplify

--- cmd/goxp/request/curl.go
+++ cmd/goxp/request/curl.go
@@ -11,13 +11,13 @@ import (
 
 func Run(ctx context.Context, url string) error {
 	resp, err := requests.Get(url).
-		Header(requests.HeaderUserAgent, userAgent()).
+		Header(requests.HeaderUserAgent, *flag.userAgent).
 		Do(ctx)
 	if err != nil {
 		return err
 	}
 
-	if verbose() {
+	if *flag.verbose {
 		fmt.Printf("%s\n", resp.Status)
 		for k := range resp.Header {
 			fmt.Printf("%s: %s\n", k, resp.Header.Get(k))
